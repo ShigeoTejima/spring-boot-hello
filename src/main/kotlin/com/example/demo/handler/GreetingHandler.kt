18281169a95443ceb7c0459c8f629a7892175c56
Shigeo Tejima <shigeo.tejima@uzabase.com>
2023-05-20 08:35:53 +0900

    modify router implementation

--- src/main/kotlin/com/example/demo/handler/GreetingHandler.kt
+++ src/main/kotlin/com/example/demo/handler/GreetingHandler.kt
@@ -2,16 +2,13 @@ package com.example.demo.handler
 
 import com.example.demo.usecase.GreetingUsecase
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.reactive.function.BodyInserters
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.*
 import reactor.core.publisher.Mono
 
 @Component
 class GreetingHandler(private val greetingUsecase: GreetingUsecase) {
 
-    @GetMapping("hello")
     fun hello(request: ServerRequest): Mono<ServerResponse> {
         val message = greetingUsecase.hello()
         return ServerResponse.ok()
