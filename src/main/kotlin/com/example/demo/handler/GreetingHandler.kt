5ae2870900aee1f441d6a25840256e908b83b0a5
Shigeo Tejima <shigeo.tejima@uzabase.com>
2023-05-20 11:20:37 +0900

    modify to use jakarta.inject for spring-boot-v3

--- src/main/kotlin/com/example/demo/handler/GreetingHandler.kt
+++ src/main/kotlin/com/example/demo/handler/GreetingHandler.kt
@@ -1,10 +1,10 @@
 package com.example.demo.handler
 
 import com.example.demo.usecase.GreetingUsecase
+import jakarta.inject.Named
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.*
 import reactor.core.publisher.Mono
-import javax.inject.Named
 
 @Named
 class GreetingHandler(private val greetingUsecase: GreetingUsecase) {
--- src/main/kotlin/com/example/demo/usecase/GreetingUsecase.kt
+++ src/main/kotlin/com/example/demo/usecase/GreetingUsecase.kt
@@ -1,6 +1,6 @@
 package com.example.demo.usecase
 
-import javax.inject.Named
+import jakarta.inject.Named
 
 @Named
 class GreetingUsecase {
