4991cb32634e999a6ae0acc3a539a82a4cd39f68
Shigeo Tejima <shigeo.tejima@uzabase.com>
2023-05-20 11:07:23 +0900

    use named

--- src/main/kotlin/com/example/demo/handler/GreetingHandler.kt
+++ src/main/kotlin/com/example/demo/handler/GreetingHandler.kt
@@ -1,12 +1,12 @@
 package com.example.demo.handler
 
 import com.example.demo.usecase.GreetingUsecase
-import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.*
 import reactor.core.publisher.Mono
+import javax.inject.Named
 
-@Component
+@Named
 class GreetingHandler(private val greetingUsecase: GreetingUsecase) {
 
     fun hello(request: ServerRequest): Mono<ServerResponse> {
