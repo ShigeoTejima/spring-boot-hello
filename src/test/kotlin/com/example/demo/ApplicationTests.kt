ec23bb8f7acb8c9544ca56752666edf6b2685909
Shigeo Tejima <shigeo.tejima@uzabase.com>
2023-05-20 15:32:16 +0900

    add application test

--- src/test/kotlin/com/example/demo/ApplicationTests.kt
+++ src/test/kotlin/com/example/demo/ApplicationTests.kt
@@ -1,13 +1,22 @@
 package com.example.demo
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
+	@Autowired lateinit var client: WebTestClient
+
 	@Test
-	fun contextLoads() {
+	fun hello() {
+		client.get().uri("/hello")
+			.exchange()
+			.expectStatus().isOk
+			.expectBody<String>().isEqualTo("Hello!")
 	}
 
 }
